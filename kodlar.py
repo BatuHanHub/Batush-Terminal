b7fffb2cdff1e07d33afc255d23176d2717a1c4b
BatuHanHub <114951572+BatuHanHub@users.noreply.github.com>
2023-02-05 18:06:02 +0300

    Update kodlar.py

--- kodlar.py
+++ kodlar.py
@@ -1,22 +1,34 @@
+from colorama import *
 import datetime
 import time
 import os
 
 isletimSistemi = os.name
-
 soru = ""
+secim = ""
 
-def sil():
+def temizle():
     if isletimSistemi == "nt":
         os.system("cls")
     elif isletimSistemi == "posix":
         os.system("clear")
-
+        
+def sil():
+    soru = str(input("Hangi dosyası silmek istersiniz?\n>>>"))
+    secim = str(input(f"{soru} dosyasını silmek istediğinizden emin misiniz? E/H >"))
+    if secim == "E":
+        os.remove(soru)
+        pass
+    else:
+        print("İşlem iptal edildi.")
+        pass
+    
 def cik():
     os.system("exit")
 
 def liste():
-    print(os.listdir())
+    for liste in os.listdir():
+        print(liste)
     
 def olustur():
     soru = str(input("Dosya adınız ne olsun ve ne formatında (.txt vs.) [dosyaadı.format]?\n>>>"))
@@ -24,21 +36,23 @@ def olustur():
     dosya.close()
 
 def yardim():
-    sil()
+    temizle()
     print("""BATUSH(Batuhan'ın Bash'i) KODLARI
 
 UYARI!: KOMUTLARINIZI KÜÇÜK HARFLERLE YAZINIZ.
 
-### DOSYA İŞLEMLERİ
+# DOSYA İŞLEMLERİ
 oluştur : dosya oluşturursunuz 
 değiş : konumunuzu değiştirir (masaüstüne gitmek için 'Masaüstü' yazın) 
 nerdeyim : şu anki konumunuzu gösterir 
 liste : konumunuzdaki dosyaları gösterir 
+sil : dosya siler
 
-### TERMİNAL KOMUTLARI
-sil : terminali temizler
+# TERMİNAL KOMUTLARI
+temizle : terminali temizler
 çık : terminalden çıkarsınız
 yardım : terminal kodlarını ve işlevlerini gösterir
+bukelemun : yazı rengi değiştirir
 
 tarih : zaman ve tarihi gösterir
 
@@ -46,7 +60,7 @@ Atatürk : deneyin :)
 bilgi : Batush hakkında bilgi verir""")
 
 def bilgi():
-    print("""Batush(Batuhan'ın Bash'i) Beta 1.0\n
+    print("""Batush(Batuhan'ın Bash'i) Beta 2.0\n
 BatuHanHub tarafından Python diliyle yazılmıştır. Sadece eğlenmek ve Python bilgimi sınamak için yazılmıştır.
 telif hakkı (c) BatuHanHub
 
@@ -73,9 +87,36 @@ def degis():
         print("HATA2: Öyle bir klasör yok")
     
 def Ataturk():
-    sil()
+    temizle()
     print("2 Dakikalık Saygı Duruşu!")
     time.sleep(120)
+    
+def bukelemun():
+    print("""Bukelemun  sürüm: 0.1 beta
+Aşağıdaki renklerden birinin sayısını giriniz. \n
+
+Kırmızı için 1
+Yeşil için 2
+Mavi için 3
+Sarı için 4 yazınız.\n""")
+    
+    secim = str(input(">>>"))
+    
+    if secim == "1":
+        print(Fore.RED + "Seçtiğiniz renk uygulandı.")
+        
+    elif secim == "2":
+        print(Fore.GREEN + "Seçtiğiniz renk uygulandı.")
+        
+    elif secim == "3":
+        print(Fore.BLUE + "Seçtiğiniz renk uygulandı.")
+        
+    elif secim == "4":
+        print(Fore.YELLOW + "Seçtiğiniz renk uygulandı.")
+        
+    else:
+        print("İşlem iptal edildi.")
+        pass
     
 def hata():
     print("HATA1: Komudunuzu doğru veya küçük yazdığınızdan emin olun.")
