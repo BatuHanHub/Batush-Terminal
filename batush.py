923a9bb8d795c7f0ef0f60fea87f9ff0055b17d1
BatuHanHub <114951572+BatuHanHub@users.noreply.github.com>
2023-04-12 02:13:46 +0300

    Update batush.py

--- batush.py
+++ batush.py
@@ -32,86 +32,81 @@ while True:
         sayi = len(kullaniciAdi)
         yer = yer[sayi:]
         
-    try: 
-        kullanici = str(input(f"{ad}@{isletimSistemiAdi.title()}-{isletimSistemiTuru.upper()}:{yer}~$"))
+    
+    kullanici = str(input(f"{ad}@{isletimSistemiAdi.title()}-{isletimSistemiTuru.upper()}:{yer}~$"))
         
-        # DOSYA İŞLEMLERİ
+    # DOSYA İŞLEMLERİ
         
-        if kullanici.startswith(olsdsy.inputKomut):
-            islev_olustur_dosya(kullanici[7:])
+    if kullanici.split()[0] == olsdsy.inputKomut:
+        islev_olustur_dosya(kullanici[7:])
         
-        elif kullanici.startswith(olskls.inputKomut):
-            islev_olustur_klasor(kullanici[7:])
+    elif kullanici.split()[0] == olskls.inputKomut:
+        islev_olustur_klasor(kullanici[7:])
             
-        elif kullanici.startswith(oku.inputKomut):
-            islev_dosyayi_oku(kullanici[4:])
+    elif kullanici.split()[0] == oku.inputKomut:
+        islev_dosyayi_oku(kullanici)
             
-        elif kullanici.startswith(sil.inputKomut):
-            islev_dosya_sil(kullanici[4:])
+    elif kullanici.split()[0] == sil.inputKomut:
+        islev_dosya_sil(kullanici[4:])
         
-        elif kullanici.startswith(silkls.inputKomut):
-            islev_sil_klasor(kullanici[7:])
+    elif kullanici.split()[0] == silkls.inputKomut:
+        islev_sil_klasor(kullanici[7:])
         
         
-        elif kullanici.startswith(git.inputKomut):
-            islev_git(kullanici[4:])
+    elif kullanici.split()[0] == git.inputKomut:
+        islev_git(kullanici[4:])
 
-        elif kullanici.startswith(nerde.inputKomut):
-            islev_nerdeyim()
+    elif kullanici.split()[0] == nerde.inputKomut:
+        islev_nerdeyim()
         
-        elif kullanici.startswith(listele.inputKomut):
-            islev_liste()
+    elif kullanici.split()[0] == listele.inputKomut:
+        islev_liste()
             
         
-        #TERMİNAL KOMUTLARI
+    #TERMİNAL KOMUTLARI
         
-        elif kullanici.startswith(temiz.inputKomut):
-            islev_temizle()
+    elif kullanici.split()[0] == temiz.inputKomut:
+        islev_temizle()
         
-        elif kullanici.startswith(cik.inputKomut):
-            islev_cik()
-            break
-        
-        elif kullanici.startswith(bukelemun.inputKomut):
-            islev_bukelemun()
+    elif kullanici.split()[0] == cik.inputKomut:
+        islev_cik()
+        break
             
+    elif kullanici.split()[0] == bilgilendir.inputKomut:
+        islev_bilgi()
             
-        elif kullanici.startswith(bilgilendir.inputKomut):
-            islev_bilgi()
+    elif kullanici.split()[0] == yardim.inputKomut:
+        islev_yardim()
             
-        elif kullanici.startswith(yardim.inputKomut):
-            islev_yardim()
-            
-        #SİSTEM KOMUTLARI 
+    #SİSTEM KOMUTLARI 
 
-        elif kullanici.startswith(calistir.inputKomut):
-            islev_calistir(kullanici[9:])
+    elif kullanici.split()[0] == calistir.inputKomut:
+        islev_calistir(kullanici[9:])
         
-        elif kullanici.startswith(kapat.inputKomut):
-            islev_kapat(kullanici[6:])
+    elif kullanici.split()[0] == kapat.inputKomut:
+        islev_kapat(kullanici[6:])
             
-        elif kullanici.startswith(tarih.inputKomut):
-            islev_tarih()
+    elif kullanici.split()[0] == tarih.inputKomut:
+        islev_tarih()
             
-        # PYTHON
-        
-        elif kullanici.startswith(python.inputKomut):
-            islev_python()
-        
-        elif kullanici.startswith(calistirPython.inputKomut):
-            islev_calistir_python(kullanici[8:])
-        
-        #EK KOMUTLAR
-        
-        elif kullanici.startswith(Ataturk.inputKomut):
-            islev_ataturk()
+    elif kullanici.split()[0] == degis.inputKomut:
+        islev_degis(kullanici[3:])
             
-        else:
-            hata()
-
-    except UnicodeDecodeError:
-        print('Hata <03> : Desteklenmeyen dosya biçimi.')
-    except FileNotFoundError:
-        print('Hata <04> : Böyle bir dosya yok.')
-    except PermissionError:
-        print('Hata <05> : Klasörü okuyamazsınız.')
+    # PYTHON
+        
+    elif kullanici.split()[0] == python.inputKomut:
+        islev_python()
+        
+    elif kullanici.split()[0] == calistirPython.inputKomut:
+        islev_calistir_python(kullanici[8:])
+        
+    #EK KOMUTLAR
+        
+    elif kullanici.split()[0] == Ataturk.inputKomut:
+        islev_ataturk()
+        
+    elif kullanici.split()[0] == kalp.inputKomut:
+        islev_kalp()
+            
+    else:
+        hata()
